6725b47f6fced606565cd4e01c39717cd821e96c
normuhamedov <normuhamedov22@gmail.com>
2024-10-13 20:18:21 +0500

    All page responsive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const headerBtn = document.querySelector('.header-btn');
-    const closeBtn = document.querySelector('.header__btn-close');
-    const navMenu = document.querySelector('.nav-menu');
+  const headerBtn = document.querySelector('.header-btn');
+  const closeBtn = document.querySelector('.header__btn-close');
+  const navMenu = document.querySelector('.nav-menu');
   
-    headerBtn.addEventListener('click', function() {
-      navMenu.classList.add('show');  // Menyuni ko'rinadigan qilib animatsiya qilish
-      headerBtn.style.display = 'none';  // Bosilgandan keyin tugma yashirinadi
-      closeBtn.style.display = 'block';  // Yopish tugmasi ko'rinadi
-    });
-  
-    closeBtn.addEventListener('click', function() {
-      navMenu.classList.remove('show');  // Menyuni qayta yashirish
-      closeBtn.style.display = 'none';  // Yopish tugmasi yashiriladi
-      headerBtn.style.display = 'block';  // Qayta ochish tugmasi ko'rinadi
-    });
-  });
-  
+headerBtn.addEventListener('click', function() {
+    navMenu.classList.add('show');
+    headerBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+});
+closeBtn.addEventListener('click', function() {
+    navMenu.classList.remove('show');
+    closeBtn.style.display = 'none';
+    headerBtn.style.display = 'block';
+});
+document.addEventListener('click', function(event) {
+  if (!navMenu.contains(event.target) && !headerBtn.contains(event.target)) {
+      navMenu.classList.remove('show');
+      closeBtn.style.display = 'none';
+      headerBtn.style.display = 'block';
+  }
+});
+});
