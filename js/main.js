21b74fd093df2eaf3ec10857f137986e76fb872d
normuhamedov <normuhamedov22@gmail.com>
2024-10-13 20:42:47 +0500

    js fixed

--- js/main.js
+++ js/main.js
@@ -5,19 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
   
 headerBtn.addEventListener('click', function() {
     navMenu.classList.add('show');
-    headerBtn.style.display = 'none';
     closeBtn.style.display = 'block';
 });
 closeBtn.addEventListener('click', function() {
     navMenu.classList.remove('show');
-    closeBtn.style.display = 'none';
-    headerBtn.style.display = 'block';
 });
 document.addEventListener('click', function(event) {
   if (!navMenu.contains(event.target) && !headerBtn.contains(event.target)) {
       navMenu.classList.remove('show');
       closeBtn.style.display = 'none';
-      headerBtn.style.display = 'block';
   }
 });
 });
